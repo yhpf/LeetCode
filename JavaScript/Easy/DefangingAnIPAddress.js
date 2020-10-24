e76ae0852dae06e10298c86396430d7564a68658
Youn Hee Pernling Frodin <yonipernling@gmail.com>
2020-10-23 20:33:37 -0400

    comment added

--- JavaScript/Easy/DefangingAnIPAddress.js
+++ JavaScript/Easy/DefangingAnIPAddress.js
@@ -3,3 +3,5 @@ var defangIPaddr = function(address) {
     address = address.replace(/\./g, "[.]"); //you have to use \. and not only . also /something/g replaces all something
     return address;
 };
+
+// would probably be faster to use a for loop and go over each character and and put into nre string, and for every . add [.]
